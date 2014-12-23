76cb96b428e4f4e7f2ee1ab4ea624acbed89af2f
Ed Copony <ecopony@yahoo.com>
2014-12-22 18:54:46 -0800

    Getting basic templating in place is preparation for some reactjs.

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 gamedayweb
 ==========
 
-Simple go webapp that utilizes the gamedayapi.
+Simple go webapp that utilizes the gamedayapi. For now, just displays the teams for 2014. More to come.
+
+    git clone git@github.com:ecopony/gamedayweb.git
+    cd gamedayweb
+    go build
+    ./gamedayweb
+
+Go to http://localhost:3000/index.html
+
--- main.go
+++ main.go
@@ -1,25 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"github.com/ecopony/gamedayapi"
 	"log"
 	"net/http"
+	"path"
+	"html/template"
 )
 
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
-	teams := gamedayapi.TeamsForYear(2014)
-	for _, team := range teams {
-		fmt.Fprintln(w, team)
-	}
+func main() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
+	http.HandleFunc("/", serveTemplate)
+
+	log.Println("Listening...")
+	http.ListenAndServe(":3000", nil)
 }
 
-func main() {
-	http.HandleFunc("/", defaultHandler)
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-		return
-	}
+func serveTemplate(w http.ResponseWriter, r *http.Request) {
+	teams := gamedayapi.TeamsForYear(2014)
+	lp := path.Join("templates", "layout.html")
+	fp := path.Join("templates", r.URL.Path)
+
+	tmpl, _ := template.ParseFiles(lp, fp)
+	tmpl.ExecuteTemplate(w, "layout", teams)
 }
